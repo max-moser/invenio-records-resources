22fc53a23b7271cc9b123e180934b279d677dca9
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2022-11-17 17:45:09 +0100

    systemfields: make IndexField smarter about search prefixes

--- invenio_records_resources/records/systemfields/index.py
+++ invenio_records_resources/records/systemfields/index.py
@@ -2,6 +2,7 @@
 #
 # Copyright (C) 2020 CERN.
 # Copyright (C) 2020 Northwestern University.
+# Copyright (C) 2022 TU Wien.
 #
 # Invenio-Records-Resources is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -12,6 +13,8 @@
 from invenio_records.systemfields import SystemField
 from invenio_search import current_search_client
 from invenio_search.engine import dsl
+from invenio_search.utils import prefix_index
+from werkzeug.local import LocalProxy
 
 
 class IndexField(SystemField):
@@ -23,10 +26,13 @@ class IndexField(SystemField):
         :param index_or_alias: An index instance or name of index/alias.
         :param search_alias: Name of alias to use for searches.
         """
+
         if isinstance(index_or_alias, dsl.Index):
             self._index = index_or_alias
         else:
-            self._index = dsl.Index(index_or_alias, using=current_search_client)
+            name = LocalProxy(lambda: prefix_index(index_or_alias))
+            self._index = dsl.Index(name, using=current_search_client)
+
         # Set search alias name directly on the index
         self._index.search_alias = search_alias or self._index._name
 
